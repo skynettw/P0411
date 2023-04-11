9eb56d2aee70f8df6c13ec92618ad4d22f0ef58a
skynettw <skynet.tw@gmail.com>
2023-04-11 11:40:04 +0800

    MyStack codes

---
+++ P0411/MyStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P0411
+{
+    class MyStack
+    {
+        private int[] data = new int[50];
+        private int top = 0;
+        public MyStack()
+        {
+            top = 0;
+        }
+        public void Push(int n)
+        {
+            if (top<50)
+            {
+                data[top++] = n;
+            } else
+            {
+                Console.WriteLine("Stack is full!");
+            }
+
+        }
+
+        public int Pop()
+        {
+            if (top>0)
+            {
+                return data[--top];
+            } else
+            {
+                Console.WriteLine("Stack is empty!");
+                return -1;
+            }
+        }
+    }
+}
