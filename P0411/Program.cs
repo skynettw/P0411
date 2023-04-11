9af55fa7d90d2f5f4b25b1951becb1b3845546d0
skynettw <skynet.tw@gmail.com>
2023-04-11 09:48:25 +0800

    加入專案檔案。

---
+++ P0411/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P0411
+{
+    class Program
+    {
+
+        // 使用遞迴來設計階乘函數
+        static int Factorial(int n)
+        {
+            if (n == 1) return 1;   // 停止自我呼叫的條件
+            return n * Factorial(n - 1);
+        }
+        static void Prog1()
+        {
+            Console.WriteLine("10!={0}",
+                Factorial(10)
+                );
+        }
+
+        static void Main(string[] args)
+        {
+            Prog1();    
+            Console.ReadKey();
+        }
+    }
+}
